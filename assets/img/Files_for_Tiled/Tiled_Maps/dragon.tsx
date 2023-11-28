e9e0c56884620c76c2af93b17f5004256bce384f
Emma Blencowe <emma.blencowe95@gmail.com>
2023-11-28 03:14:14 +0000

    Manually changed collision values to 26 for all levels

---
+++ assets/img/Files_for_Tiled/Tiled_Maps/dragon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="dragon" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../sprites_tile_sheets/dragon.png" width="128" height="128"/>
+</tileset>
